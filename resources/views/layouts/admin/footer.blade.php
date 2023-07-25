08e0180b7c384b273f2fa8f346d228be094a1fd4
zaazxz <mirzaqamaruzzaman18@gmail.com>
2023-07-25 14:40:04 +0700

    Fixed Footer

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
-        <div class="footer-left">
+        <div class="footer-left" id="footerLeftMain">
           Copyright &copy; 2022 <a href="https://www.linkedin.com/in/rioandrianto/" target="_blank">IoTech Studio</a>
         </div>
-        <div class="footer-right">
+        <div class="footer-right" id="footerRightMain">
           SIM Presensi is Part of SIMBA - Made with &#10084;&#65039; from Bandung
         </div>
 </footer>
